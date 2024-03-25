679d55c7555fdb24c6d7d0b33f0be2958c7e63d7
Roman <romalupan89@gmail.com>
2024-03-25 21:34:10 +0200

    get projects

--- app/hub/page.tsx
+++ app/hub/page.tsx
@@ -1,8 +1,14 @@
+import { createClient } from '@/utils/supabase/server';
+
 export default async function Hub() {
+  const supabase = createClient();
+  let { data: Projects, error } = await supabase.from('Projects').select('*');
+
   return (
     <>
       <div>
-        <h1>Protected route</h1>
+        <h1>Get Projects</h1>
+        <pre>{JSON.stringify(Projects, null, 2)}</pre>
       </div>
     </>
   );
--- app/workspace/[id]/dashboard/page.tsx
+++ app/workspace/[id]/dashboard/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 type Props = {};
 
 function page({}: Props) {
-  return <div className="dash">dash</div>;
+  return <div className="">dash</div>;
 }
 
 export default page;
