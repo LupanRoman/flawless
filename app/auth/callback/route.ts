b6b82182898ee7ea06a1a3ddd7d81bee2802a533
Roman <romalupan89@gmail.com>
2024-04-12 13:45:21 +0300

    commit

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -15,6 +15,6 @@ export async function GET(request: Request) {
   }
 
   // URL to redirect to after sign up process completes
-  return NextResponse.redirect(`${origin}/hub`);
-  // return NextResponse.redirect(`https://flawless-omega.vercel.app/hub`);
+  // return NextResponse.redirect(`${origin}/hub`);
+  return NextResponse.redirect(`https://flawless-omega.vercel.app/hub`);
 }
