d5164acdcb247a74c241bcdbed67f633c05899e3
Roman <romalupan89@gmail.com>
2024-04-18 21:37:45 +0300

    changes

--- app/workspace/[id]/tasks/[slug]/page.tsx
+++ app/workspace/[id]/tasks/[slug]/page.tsx
@@ -1,13 +1,19 @@
 import EditTask from "@/components/workspace/tasks/editTask";
+import { createClient } from "@/utils/supabase/server";
 import React from "react";
 
 type Props = {};
 
-function page({ params: { slug } }: { params: { slug: number } }) {
+async function page({ params: { slug } }: { params: { slug: number } }) {
+  const supabase = createClient();
+  const { data: TaskGroupID, error } = await supabase
+    .from("Tasks")
+    .select("group_id")
+    .eq("id", slug);
   return (
     <>
       <div>
-        <EditTask slug={slug} />
+        <EditTask slug={slug} taskGroupID={TaskGroupID} />
       </div>
     </>
   );
